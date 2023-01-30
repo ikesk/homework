448797e1a64d5c5661451b49a9aad88332006f54
ikesk <ikeskin@ucdavis.edu>
2023-01-29 17:30:37 -0800

    init

---
+++ 25atseq.py
@@ -0,0 +1,20 @@
+# 25atseq.py
+
+# Write a program that stores random DNA sequence in a string
+# The sequence should be 30 nt long
+# On average, the sequence should be 60% AT
+# Calculate the actual AT fraction while generating the sequence
+# Report the length, AT fraction, and sequence
+
+# Note: set random.seed() if you want repeatable random numbers
+
+import random
+
+nucleotides = ['A', 'T', 'C', 'G']
+
+print(random.choices(nucleotides)
+      
+"""
+python3 25atseq.py
+30 0.6666666666666666 ATTACCGTAATCTACTATTAAGTCACAACC
+"""
