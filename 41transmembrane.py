8dfb7782995ae0dce0c83568ad2f9a0449d2993f
ikesk <ikeskin@ucdavis.edu>
2023-11-28 12:24:32 -0800

    init

--- 41transmembrane.py
+++ 41transmembrane.py
@@ -20,8 +20,13 @@
 # Hint: create a function for KD calculation
 # Hint: create a function for hydrophobic alpha-helix
 # Hint: use the same function for both signal peptide and transmembrane
-import sys
+
 import mcb185
+
+kd_scores = {'I': 4.5, 'V': 4.2, 'L': 3.8, 'F': 2.8, 'C': 2.5,
+             'M': 1.9, 'A': 1.8, 'G': -0.4, 'T': -0.7, 'S': -0.8,
+             'W': -0.9, 'Y': -1.3, 'P': -1.6, 'H': -3.2, 'E': -3.5,
+             'Q': -3.5, 'D': -3.5, 'N': -3.5,'K': -3.9, 'R': -4.5}
 
 """
 python3 41transmembrane.py ~/DATA/E.coli/GCF_000005845.2_ASM584v2_protein.faa.gz
